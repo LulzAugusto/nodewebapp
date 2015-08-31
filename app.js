00988137437dae41d9197e81df7a303bb4294b11
Luiz Augusto Crisostomo <luiz66@gmail.com>
2015-08-30 23:30:13 -0300

    Added a basic server

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+const express    = require('express');        // call express
+const app        = express();                 // define our app using express
+const bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 9001;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+const router = express.Router();              // get an instance of the express Router
+
+// test route to make sure everything is working (accessed at GET http://localhost:9001/api)
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port);
+console.log('Magic happens on port ' + port);
